b062c58e4cf83d2c0a0455f5a6dfb7b1317cb8d2
AsheniPerera <asperera@wiley.com>
2021-08-17 15:27:05 +0530

    Changing moodleCI file

--- testcode.php
+++ testcode.php
@@ -1,24 +1,25 @@
+
 <?php
 
-class SimpleClass
-{
+class SimpleClass {
+
     // property declaration
-    public string $name = 'Hi My Name is Ash';
+    public string $name = "Hi My Name is Ash";
     public int $number = 25;
 
     // method declaration
-    public function displayVar(): void
-    {
+    public function displayvar(): void {
+
         for ($count = 0; $count <= 10; $count++) {
             echo $this->name;
         }
     }
 
     // method declaration
-    public function displayNum(): void
-    {
+    public function displaynum(): void {
+
         echo "This is a Number" . $this->number;
-        //my number
-        //comment on me
+        // my number
+        // comment on me
     }
 }
