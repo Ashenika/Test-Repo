515615ffff8823f6794be3c77df1658fe647d455
AsheniPerera <asperera@wiley.com>
2021-08-17 13:41:02 +0530

    Adding php file

---
+++ testcode.php
@@ -0,0 +1,24 @@
+<?php
+
+class SimpleClass
+{
+    // property declaration
+    public string $name = 'Hi My Name is Ash';
+    public int $number = 25;
+
+    // method declaration
+    public function displayVar(): void
+    {
+        for ($count = 0; $count <= 10; $count++) {
+            echo $this->name;
+        }
+    }
+
+    // method declaration
+    public function displayNum(): void
+    {
+        echo "This is a Number" . $this->number;
+        //my number
+        //comment on me
+    }
+}
