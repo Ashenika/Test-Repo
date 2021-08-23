aab27e7e29d966f89871951b45068d618a97c0d9
WILEY\vrajarathn <vrajarathn@wiley.com>
2021-08-23 11:32:14 +0530

    test commit 7

--- lang/en/local_test.php
+++ lang/en/local_test.php
@@ -17,9 +17,9 @@
 /**
  * Version file for Facilitation Dashboard
  *
- * @package    local_test
- * @copyright  2021 Wiley Educational Services
- * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package local_test
+ * @copyright 2021 Wiley Educational Services
+ * @license https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
  * $plugin->version
  * The current version number of the plugin. The format is partially date
