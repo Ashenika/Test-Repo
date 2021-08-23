fe842e6a1de1ba92034b56f8302bd9ce7ed16f1a
WILEY\vrajarathn <vrajarathn@wiley.com>
2021-08-23 11:39:38 +0530

    test commit 9

--- lang/en/local_test.php
+++ lang/en/local_test.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * This file is part of Moodle - http://moodle.org/
  *
  * Moodle is free software: you can redistribute it and/or modify
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 /**
  * Version file for Facilitation Dashboard
