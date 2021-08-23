e927e2d5ee600b9ee82b09bbc5d5a78d872e24ef
AsheniPerera <asperera@wiley.com>
2021-08-23 15:34:26 +0530

    checked with phpcs

--- lang/en/local_test.php
+++ lang/en/local_test.php
@@ -1,6 +1,5 @@
 <?php
 
-/**
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,7 +14,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 /**
  * Version file for Facilitation Dashboard
