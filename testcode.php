95dbaf87890cd41827c06f08ca73d119c2f3c608
WILEY\vrajarathn <vrajarathn@wiley.com>
2021-08-23 11:22:31 +0530

    test commit 5

--- testcode.php
+++ testcode.php
@@ -16,23 +16,20 @@
 
 class SimpleClass {
 
-    // property declaration
-    public string $name = "Hi My Name is Ash";
-    public int $number = 25;
+    // Property declaration.
+    public $name = "Hi My Name is Ash";
+    public $number = 25;
 
-    // method declarationg
-    public function displayvar(): void {
+    // Method declarationg.
+    public function displayvar() {
 
         for ($count = 0; $count <= 10; $count++) {
             echo $this->name;
         }
     }
 
-    // method declaration
-    public function displaynum(): void {
-
+    // Method declaration.
+    public function displaynum() {
         echo "This is a Number" . $this->number;
-        // my number
-        // comment on me
     }
 }
