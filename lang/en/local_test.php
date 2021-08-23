32666a56bf5e8194d02c06d4c4c52b86febd2898
AsheniPerera <asperera@wiley.com>
2021-08-23 15:05:45 +0530

    Testcommit Adding CSFixer

--- lang/en/local_test.php
+++ lang/en/local_test.php
@@ -1,4 +1,5 @@
 <?php
+/**
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -13,6 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /**
  * Version file for Facilitation Dashboard
